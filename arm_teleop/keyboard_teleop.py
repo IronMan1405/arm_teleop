b60fa002bb842ffc925ec5c6c80cf88e5e6e78b4
Dakshesh Nankani <daksheshnankani1405@gmail.com>
2026-02-05 14:10:46 +0530

    added few more commands

--- arm_teleop/keyboard_teleop.py
+++ arm_teleop/keyboard_teleop.py
@@ -2,7 +2,7 @@ import rclpy
 from rclpy.node import Node
 
 from geometry_msgs.msg import Vector3
-from std_msgs.msg import Bool
+from std_msgs.msg import Bool, String
 
 import sys, termios, tty, threading
 
@@ -12,15 +12,18 @@ class KeyboardTeleop(Node):
 
         self.delta_pub = self.create_publisher(Vector3, '/arm/teleop_delta', 10)
         self.ori_pub = self.create_publisher(Bool, '/arm/orientation_constraint', 10)
+        self.named_pose_pub = self.create_publisher(String, '/arm/named_pose', 10)
 
-        self.step = 0.02
+        self.step = 0.05
         self.ori_constrained = False
+        self.pending_delta = Vector3()
+
 
         self.get_logger().info(
             "\nKeyboard teleop started\n"
             "[W/S]: +X/-X | [A/D]: +Y/-Y | [Q/E]: +Z/-Z\n"
             "[C]: enable/disable orientation constaint\n"
-            "[H]: home | [ESC]: exit"
+            "[O]: Zero Pose | [H]: home | [ESC]: exit"
         )
 
         self._stop = False
@@ -39,20 +42,60 @@ class KeyboardTeleop(Node):
 
                 delta = Vector3()
 
+                # scale = 3.0
+
                 if key == "w":
                     delta.x = self.step
+                    # delta.x *= scale
+                    # self.pending_delta.x += self.step * scale
                 elif key == "s":
                     delta.x = -self.step
+                    # delta.x *= scale
+                    # self.pending_delta.x -= self.step * scale
                 elif key == "a":
                     delta.y = self.step
+                    # delta.y *= scale
+                    # self.pending_delta.y += self.step * scale
                 elif key == "d":
                     delta.y = -self.step
+                    # delta.y *= scale
+                    # self.pending_delta.y -= self.step * scale
                 elif key == "q":
                     delta.z = self.step
+                    # delta.z *= scale
+                    # self.pending_delta.z += self.step * scale
                 elif key == "e":
                     delta.z = -self.step
-                # elif key == "h":
-                    # go home
+                    # delta.z *= scale
+                    # self.pending_delta.z -= self.step * scale
+
+                elif key == "h":
+                    msg = String()
+                    msg.data = 'home'
+                    self.named_pose_pub.publish(msg)
+                    self.get_logger().info("Sent HOME pose")
+                    continue
+                elif key == 'o':
+                    msg = String()
+                    msg.data = 'zero'
+                    self.named_pose_pub.publish(msg)
+                    self.get_logger().info("Sent ZERO pose")
+                    continue
+
+                elif key == 'f':
+                    msg = String()
+                    msg.data = 'front'
+                    self.named_pose_pub.publish(msg)
+                    self.get_logger().info("Sent FRONT pose")
+                    continue
+
+                elif key == 'b':
+                    msg = String()
+                    msg.data = 'back'
+                    self.named_pose_pub.publish(msg)
+                    self.get_logger().info("Sent BACK pose")
+                    continue
+                
                 
                 elif key == 'c':
                     self.ori_constrained = not self.ori_constrained
@@ -69,6 +112,17 @@ class KeyboardTeleop(Node):
                     self._stop = True
                     rclpy.shutdown()
                     break
+
+                # elif key == ' ':
+                #     self.delta_pub.publish(self.pending_delta)
+                #     self.get_logger().info(
+                #         f"Executed delta: "
+                #         f"x={self.pending_delta.x:.3f}, "
+                #         f"y={self.pending_delta.y:.3f}, "
+                #         f"z={self.pending_delta.z:.3f}"
+                #     )
+                #     self.pending_delta = Vector3()
+                #     continue
 
                 else:
                     continue
